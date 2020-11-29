50e8ff9be22f92c5b2c6255abe656e11bc5e896a
Bawantha Dissanayake <bawanthadissanayake@gmail.com>
2020-11-29 20:04:11 +0530

    Add customer modifications

--- dashNew1/Add_vehicle.xaml.cs
+++ dashNew1/Add_vehicle.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.IO;
 
 namespace dashNew1
 {
@@ -22,6 +24,22 @@ namespace dashNew1
         public Add_vehicle()
         {
             InitializeComponent();
+        }
+
+        Connect_DB db = new Connect_DB();
+        string filepath;
+
+        private String GetDestinationPath(string filename)
+        {
+            String appStartPath = System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            string dir = appStartPath + "\\" + txt_lno.Text;
+            // If directory does not exist, create it
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            appStartPath = String.Format(dir + "\\" + txt_lno.Text + ".jpg");
+            return appStartPath;
         }
     }
 }
