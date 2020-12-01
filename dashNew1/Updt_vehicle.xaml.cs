703e80120feecf6a6c81561033e5fbe1cb52b910
Abhishek Chathumal <acwellage@gmail.com>
2020-12-01 08:33:39 +0530

    added insuarance,owner forms

---
+++ dashNew1/Updt_vehicle.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace dashNew1
+{
+    /// <summary>
+    /// Interaction logic for Updt_vehicle.xaml
+    /// </summary>
+    public partial class Updt_vehicle : Window
+    {
+        public Updt_vehicle()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
