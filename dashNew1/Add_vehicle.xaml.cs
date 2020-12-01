bc3e7b07ecdbcb801d80200fdba23a811277d01b
Bawantha Dissanayake <bawanthadissanayake@gmail.com>
2020-12-01 11:34:47 +0530

    edits

--- dashNew1/Add_vehicle.xaml.cs
+++ dashNew1/Add_vehicle.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using System.IO;
+using System.Data;
+using Microsoft.Win32;
 
 namespace dashNew1
 {
@@ -52,6 +54,30 @@ namespace dashNew1
         {
             Add_Insurnce obj = new Add_Insurnce();
             obj.Show();
+        }
+
+        private void Vehicle_Setup_Form_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = db.getData("select * from Owner");
+            cbox_oid.ItemsSource = dt.DefaultView;
+            cbox_oid.DisplayMemberPath = "O_ID";
+            cbox_oid.SelectedValuePath = "O_ID";
+        }
+
+        private void btn_upload_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Multiselect = false;
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            bool? result = open.ShowDialog();
+
+            if (result == true)
+            {
+                filepath = open.FileName; // Stores Original Path in Textbox    
+                ImageSource imgsource = new BitmapImage(new Uri(filepath)); // Just show The File In Image when we browse It
+                img_vehicle.Source = imgsource;
+            }
         }
     }
 }
