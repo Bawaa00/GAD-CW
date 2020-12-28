fcf4b6b50748f949f77078dcbddca1bf9db81cee
Bawantha Dissanayake <bawanthadissanayake@gmail.com>
2020-12-28 09:50:33 +0530

    button save

--- dashNew1/Update_ownr.xaml.cs
+++ dashNew1/Update_ownr.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
 
 namespace dashNew1
 {
@@ -26,7 +29,21 @@ namespace dashNew1
         }
         Connect_DB db = new Connect_DB();
         string path;
-       
+        string newpath;
+
+        private String GetDestinationPath(string filename)
+        {
+            String appStartPath = System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            string dir = appStartPath + "\\" + cmb_oid.Text;
+            // If directory does not exist, create it
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            appStartPath = String.Format(dir + "\\" + cmb_oid.Text + ".jpg");
+            return appStartPath;
+        }
+
         private void form_up_owner_Loaded(object sender, RoutedEventArgs e)
         {
             DataTable dt = new DataTable();
@@ -47,6 +64,40 @@ namespace dashNew1
             path = dt.Rows[0][5].ToString();
             ImageSource imgsource = new BitmapImage(new Uri(path));
             img_owner.Source = imgsource;
+        }
+
+        private void btn_upload_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Multiselect = false;
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            bool? result = open.ShowDialog();
+
+            if (result == true)
+            {
+                path = open.FileName; // Stores Original Path in Textbox    
+                ImageSource imgsource = new BitmapImage(new Uri(path)); // Just show The File In Image when we browse It
+                img_owner.Source = imgsource;
+            }
+        }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            string a = " update Owner set  O_ID= '" + cmb_oid.Text + "', O_NIC = '" + txt_nic.Text + "', " +
+                           " O_Name=  '" + txt_name.Text + "', O_Address= '" + txt_address.Text + "',O_Tel = '"+txt_contact.Text+"' ,O_path = '" + path + "' where Driver_ID  = '" + cmb_oid.Text + "'";
+
+            string name = System.IO.Path.GetFileName(path);
+            string destinationPath = GetDestinationPath(name);
+
+            File.Copy(path, destinationPath, true);
+            //txt_did.Text = System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+
+            int line = db.save_update_delete(a);
+            if (line == 1)
+                MessageBox.Show("Data save Successfully", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            else
+                MessageBox.Show("Data cannot save", "error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
