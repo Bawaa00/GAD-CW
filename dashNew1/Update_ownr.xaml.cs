cc98db7aaae2044adaa0dce32754796dbd483246
Bawantha Dissanayake <bawanthadissanayake@gmail.com>
2020-12-28 09:37:32 +0530

    drop down close method

--- dashNew1/Update_ownr.xaml.cs
+++ dashNew1/Update_ownr.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace dashNew1
 {
@@ -22,6 +23,30 @@ namespace dashNew1
         public Update_ownr()
         {
             InitializeComponent();
+        }
+        Connect_DB db = new Connect_DB();
+        string path;
+       
+        private void form_up_owner_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = db.getData("select * from Owner");
+            cmb_oid.ItemsSource = dt.DefaultView;
+            cmb_oid.DisplayMemberPath = "O_ID";
+            cmb_oid.SelectedValuePath = "O_ID";
+        }
+
+        private void cmb_oid_DropDownClosed(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = db.getData("select * from Owner where O_ID = '"+cmb_oid.Text+"'");
+            txt_nic.Text = dt.Rows[0][1].ToString();
+            txt_name.Text = dt.Rows[0][2].ToString();
+            txt_address.Text = dt.Rows[0][3].ToString();
+            txt_contact.Text = dt.Rows[0][4].ToString();
+            path = dt.Rows[0][5].ToString();
+            ImageSource imgsource = new BitmapImage(new Uri(path));
+            img_owner.Source = imgsource;
         }
     }
 }
